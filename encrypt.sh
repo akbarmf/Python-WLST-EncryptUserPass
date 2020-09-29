cac12b571a7c68d6ae086502414325988c6876ae
akbarmf <faisalmmakbar@gmail.com>
2020-09-29 09:48:52 +0700

    add sh script

---
+++ encrypt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+
+#Define WebLogic binary path
+WL_HOME=/bcaibank/bin/wls/12.2.1.3/oracle_common
+
+#Define script location path
+SCRIPT_LOC="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#Define script and properties name
+py=encrypt.py
+prop=encrypt.properties
+
+
+#Running wlst
+"${WL_HOME}"/common/bin/wlst.sh "${SCRIPT_LOC}"/"${py}" "${SCRIPT_LOC}"/"${prop}"
+
+
+#Get target path from properties
+targetPath="$( cat "${SCRIPT_LOC}"/"${prop}" | grep "exportFilePath" | grep -v "#" | cut -d "=" -f 2 )"
+
+#Change permission target dir
+echo -e "[INFO] Change Directory Target Permission to 700\n"
+chmod 700 "${targetPath}"
